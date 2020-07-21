e2235b07d4f25d5e91cf151bd59679064ee39545
Jordan Cannon <LiteracyFanatic@gmail.com>
2020-07-21 18:02:06 -0500

    Initial build

---
+++ furigana.js
@@ -0,0 +1,14 @@
+const furigana = document.getElementsByTagName("rt");
+const inputs = document.getElementsByName('furigana');
+
+function updateFurigana(furiganaType) {
+    for (const rt of furigana) {
+        rt.innerText = rt.dataset[furiganaType] || "";
+    }
+}
+
+for (const radio of inputs) {
+    radio.addEventListener("change", event => {
+        updateFurigana(event.target.value);
+    });
+}
